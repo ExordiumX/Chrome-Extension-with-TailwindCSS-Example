30c63d5b9c145d2a4119a744c5439ac5476ae046
DanDev <dandev95@gmail.com>
2021-09-01 17:46:48 +0200

    first commit

---
+++ dist/background.js
@@ -0,0 +1,182 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+(function(modules, entry, mainEntry, parcelRequireName, globalName) {
+  /* eslint-disable no-undef */
+  var globalObject =
+    typeof globalThis !== 'undefined'
+      ? globalThis
+      : typeof self !== 'undefined'
+      ? self
+      : typeof window !== 'undefined'
+      ? window
+      : typeof global !== 'undefined'
+      ? global
+      : {};
+  /* eslint-enable no-undef */
+
+  // Save the require from previous bundle to this closure if any
+  var previousRequire =
+    typeof globalObject[parcelRequireName] === 'function' &&
+    globalObject[parcelRequireName];
+
+  var cache = previousRequire.cache || {};
+  // Do not use `require` to prevent Webpack from trying to bundle this call
+  var nodeRequire =
+    typeof module !== 'undefined' &&
+    typeof module.require === 'function' &&
+    module.require.bind(module);
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire =
+          typeof globalObject[parcelRequireName] === 'function' &&
+          globalObject[parcelRequireName];
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error("Cannot find module '" + name + "'");
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = (cache[name] = new newRequire.Module(name));
+
+      modules[name][0].call(
+        module.exports,
+        localRequire,
+        module,
+        module.exports,
+        this
+      );
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x) {
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x) {
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function(id, exports) {
+    modules[id] = [
+      function(require, module) {
+        module.exports = exports;
+      },
+      {},
+    ];
+  };
+
+  Object.defineProperty(newRequire, 'root', {
+    get: function() {
+      return globalObject[parcelRequireName];
+    },
+  });
+
+  globalObject[parcelRequireName] = newRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (mainEntry) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(mainEntry);
+
+    // CommonJS
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+      module.exports = mainExports;
+
+      // RequireJS
+    } else if (typeof define === 'function' && define.amd) {
+      define(function() {
+        return mainExports;
+      });
+
+      // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+})({"bMCAy":[function(require,module,exports) {
+var _crxHotreload = require("crx-hotreload");
+
+},{"crx-hotreload":"8Jj1f"}],"8Jj1f":[function(require,module,exports) {
+const filesInDirectory = (dir)=>new Promise((resolve)=>dir.createReader().readEntries((entries)=>Promise.all(entries.filter((e)=>e.name[0] !== '.'
+            ).map((e)=>e.isDirectory ? filesInDirectory(e) : new Promise((resolve1)=>e.file(resolve1)
+                )
+            )).then((files)=>[].concat(...files)
+            ).then(resolve)
+        )
+    )
+;
+const timestampForFilesInDirectory = (dir)=>filesInDirectory(dir).then((files)=>files.map((f)=>f.name + f.lastModifiedDate
+        ).join()
+    )
+;
+const watchChanges = (dir, lastTimestamp)=>{
+    timestampForFilesInDirectory(dir).then((timestamp)=>{
+        if (!lastTimestamp || lastTimestamp === timestamp) setTimeout(()=>watchChanges(dir, timestamp)
+        , 1000) // retry after 1s
+        ;
+        else chrome.runtime.reload();
+    });
+};
+chrome.management.getSelf((self)=>{
+    if (self.installType === 'development') {
+        chrome.runtime.getPackageDirectoryEntry((dir)=>watchChanges(dir)
+        );
+        chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true
+        }, (tabs)=>{
+            if (tabs[0]) chrome.tabs.reload(tabs[0].id);
+        });
+    }
+});
+
+},{}]},["bMCAy"], "bMCAy", "parcelRequire8e97")
+
+//# sourceMappingURL=background.js.map
